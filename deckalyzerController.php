e9fd4fe1ed287be21c15f8ab606b8dede8dd3dc1
Warren Allen IV <38433765+War1412@users.noreply.github.com>
2018-10-23 15:37:03 -0500

    Update deckalyzerController.php

--- deckalyzerController.php
+++ deckalyzerController.php
@@ -67,14 +67,12 @@
 					print $this->views->loginFormView($this->message);
 					break;
 				default: // 'cardlist'
-					//list($orderBy, $orderDirection) = $this->model->getOrdering();
 					list($cards, $error) = $this->model->getCardCollection();
 					if ($error) {
 						$this->message = $error;
 					}
-					//print $this->views->cardListView($cards, $orderBy, $orderDirection, $this->message);
-
-list($decks, $error) = $this->model->getDeckCollection();
+					
+					list($decks, $error) = $this->model->getDeckCollection();
 					if($error)
 					{
 						$this->message = $error;	
